ac8f9d7bf236bc0c993d09ecd579532cbf1fbbf1
bebecool <tonny76600@gmail.com>
2025-09-15 19:50:23 +0200

    Remplacer nom boutique

--- storefront/src/app/[countryCode]/(main)/categories/[...category]/page.tsx
+++ storefront/src/app/[countryCode]/(main)/categories/[...category]/page.tsx
@@ -57,7 +57,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
       `${title} category.`
 
     return {
-      title: `${title} | Medusa Store`,
+      title: `${title} | Neobloby Store`,
       description,
       alternates: {
         canonical: `${params.category.join("/")}`,

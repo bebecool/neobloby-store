adda0d13e08c931bd577ddde724906ba715db172
bebecool <tonny76600@gmail.com>
2025-09-15 22:02:10 +0200

    fix i18n issues in hero component

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -1,21 +1,17 @@
 import { Heading, Button } from "@medusajs/ui";
 
-import { useTranslation } from "react-i18next"
-import "@lib/i18n"
-
 const Hero = () => {
-  const { t } = useTranslation();
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
         <Heading level="h1" className="text-3xl leading-10 text-ui-fg-base font-normal">
-          {t("hero_title")}
+          Kits & sclérotes de blob
         </Heading>
         <Heading level="h2" className="text-xl leading-8 text-ui-fg-subtle font-normal">
-          {t("hero_subtitle")}
+          Des blobo (Physarum polycephalum) pour vos expériences scientifiques !
         </Heading>
         <Button className="mt-4">
-          {t("hero_button")}
+          Acheter
         </Button>
       </div>
     </div>

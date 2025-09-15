9b33a179b7a72ee6408ba2e33187719bdf4973ac
bebecool <tonny76600@gmail.com>
2025-09-15 22:11:53 +0200

    remove i18n encore

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -1,20 +1,17 @@
 import { Heading, Button } from "@medusajs/ui";
-import { useI18nTranslation } from "@lib/hooks/useI18nTranslation";
 
 const Hero = () => {
-  const { t } = useI18nTranslation();
-  
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
         <Heading level="h1" className="text-3xl leading-10 text-ui-fg-base font-normal">
-          {t("hero_title")}
+          Kits & sclérotes de blob
         </Heading>
         <Heading level="h2" className="text-xl leading-8 text-ui-fg-subtle font-normal">
-          {t("hero_subtitle")}
+          Des blobo (Physarum polycephalum) pour vos expériences scientifiques !
         </Heading>
         <Button className="mt-4">
-          {t("hero_button")}
+          Acheter
         </Button>
       </div>
     </div>
--- storefront/src/modules/layout/templates/nav/index.tsx
+++ storefront/src/modules/layout/templates/nav/index.tsx
@@ -3,12 +3,8 @@ import { Suspense } from "react"
 import { listRegions } from "@lib/data/regions"
 import { StoreRegion } from "@medusajs/types"
 import LocalizedClientLink from "@modules/common/components/localized-client-link"
-
 import CartButton from "@modules/layout/components/cart-button"
 import SideMenu from "@modules/layout/components/side-menu"
-
-import dynamic from "next/dynamic"
-const LanguageSwitcher = dynamic(() => import("@modules/layout/components/language-switcher"), { ssr: false })
 
 export default async function Nav() {
   const regions = await listRegions().then((regions: StoreRegion[]) => regions)
@@ -34,7 +30,6 @@ export default async function Nav() {
           </div>
 
           <div className="flex items-center gap-x-6 h-full flex-1 basis-0 justify-end">
-            <LanguageSwitcher />
             <div className="hidden small:flex items-center gap-x-6 h-full">
               {process.env.NEXT_PUBLIC_FEATURE_SEARCH_ENABLED && (
                 <LocalizedClientLink

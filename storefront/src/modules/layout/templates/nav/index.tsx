99a8fb0a886eb1db7905a6c034d24abcc3cf292c
bebecool <tonny76600@gmail.com>
2025-09-16 12:33:19 +0200

    refactor: suppression i18n, retour version stable

--- storefront/src/modules/layout/templates/nav/index.tsx
+++ storefront/src/modules/layout/templates/nav/index.tsx
@@ -1,14 +1,10 @@
 import { Suspense } from "react"
-import dynamic from "next/dynamic"
 
 import { listRegions } from "@lib/data/regions"
 import { StoreRegion } from "@medusajs/types"
 import LocalizedClientLink from "@modules/common/components/localized-client-link"
 import CartButton from "@modules/layout/components/cart-button"
 import SideMenu from "@modules/layout/components/side-menu"
-
-// Import dynamique du sélecteur de langue pour éviter les problèmes SSR
-const LanguageSwitcher = dynamic(() => import("@modules/layout/components/language-switcher"), { ssr: false })
 
 export default async function Nav() {
   const regions = await listRegions().then((regions: StoreRegion[]) => regions)
@@ -34,7 +30,6 @@ export default async function Nav() {
           </div>
 
           <div className="flex items-center gap-x-6 h-full flex-1 basis-0 justify-end">
-            <LanguageSwitcher />
             <div className="hidden small:flex items-center gap-x-6 h-full">
               {process.env.NEXT_PUBLIC_FEATURE_SEARCH_ENABLED && (
                 <LocalizedClientLink
